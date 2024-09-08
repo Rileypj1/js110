0e582aef00594cdfe119c8b3603a7820f11e1bbc
Rileypj <11priley@gmail.com>
2024-09-07 21:36:11 -0400

    starting js110 lesson 1

---
+++ lesson_1/sample_exercises.js
@@ -0,0 +1,9 @@
+// let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.join('')); // => 'a,b,c,d,e,f'
+
+let obj = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' }
+obj['apple'] = 'Fruit'
+obj.carrot = 'Vegetable'
+console.log(obj);
+obj['pear'] = 'Fruit';
+console.log(obj);
