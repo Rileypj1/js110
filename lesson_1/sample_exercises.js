7f44bfe453e72eadfcc0558f8151715722d8cd67
Rileypj <11priley@gmail.com>
2024-09-08 21:55:24 -0400

    going through lesson 1 sample exercises

--- lesson_1/sample_exercises.js
+++ lesson_1/sample_exercises.js
@@ -4,6 +4,25 @@
 let obj = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' }
 obj['apple'] = 'Fruit'
 obj.carrot = 'Vegetable'
-console.log(obj);
 obj['pear'] = 'Fruit';
-console.log(obj);
+
+function selectFruit(produce) {
+  let fruit = {};
+  for (i in produce) {
+    if (produce[i] === 'Fruit'){
+      fruit[i] = produce[i];
+    }
+  }
+  return fruit;
+}
+
+function multiply(numbers, multiplier) {
+  let newNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let currentNum = numbers[i]
+    newNumbers.push(currentNum * multiplier);
+  }
+  console.log(newNumbers);
+}
+let myNumbers = [1, 4, 3, 7, 2, 6];
+multiply(myNumbers, 3); // => [3, 12, 9, 21, 6, 18]
