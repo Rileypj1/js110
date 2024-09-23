096728ed1a1968f6b006d7ca4fdf9e14ed1b382e
Rileypj <11priley@gmail.com>
2024-09-22 20:51:19 -0400

    working thorugh easy 6

--- lesson_2/small-problems/easy6.js
+++ lesson_2/small-problems/easy6.js
@@ -47,3 +47,98 @@ reverseNumber(456);      // 654
 reverseNumber(12000);    // 21 -- Note that leading zeros in the result get dropped!
 reverseNumber(1);        // 1
 */
+/*
+// Counting Up
+function sequence(int) {
+  let result = []
+  let i = 1
+  do {
+    result.push(i)
+    i += 1;
+  } while (i <= int)
+  console.log(result);
+}
+
+sequence(5);    // [1, 2, 3, 4, 5]
+sequence(3);    // [1, 2, 3]
+sequence(1);    // [1]
+*/
+/*
+// Name Swapping
+function swapName(name) {
+  console.log(name.split(' ').reverse().join(', '));
+}
+swapName('Joe Roberts');    // "Roberts, Joe"
+*/
+/*
+//Sequence Count
+function sequence(count, startingNum) {
+  let result = [];
+  let i = 1;
+  while (result.length < count) {
+    result.push(startingNum * i)
+    i += 1;
+  }
+  console.log(result);
+}
+sequence(5, 1);          // [1, 2, 3, 4, 5]
+sequence(4, -7);         // [-7, -14, -21, -28]
+sequence(3, 0);          // [0, 0, 0]
+sequence(0, 1000000);    // []
+*/
+/*
+// Reverse It (part 1 & 2)
+
+// function reverseSentence(str) {
+//   console.log(str.split(' ').reverse().join(' '));
+// }
+// reverseSentence('');                       // ""
+// reverseSentence('Hello World');            // "World Hello"
+// reverseSentence('Reverse these words');    // "words these Reverse"
+
+function reverseWords(str) {
+  let final = str.split(' ').map(word => {
+    if (word.length >= 5) {
+      return word.split('').reverse().join('');
+    } else return word;
+  });
+  console.log(final);
+}
+reverseWords('Professional');             // "lanoisseforP"
+reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
+*/
+
+// Matching Parentheses
+// 1. find instances of all '(' and ')' in the input string 
+// 2. push the index values of '(' to an array called Left
+// 3. push the index values of ')' to a separate array called Right
+// 4. if the length of Left doesn't equal right, return false
+// 5. Else, iterate over the Left array. For each index value
+//   - there should be a greater index value in the Right array
+//   - if this is condition is met for every index value, return true, else false
+
+function isBalanced(str) {
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === '(') {
+      left.push(i);
+    } else if (str[i] === ')') {
+      right.push(i);
+    }
+  }
+
+  if (left.length !== right.length) return false;
+  return left.every((paren, idx) => left[idx] < right[idx]);
+}
+
+console.log(isBalanced("What (is) this?") === true);
+console.log(isBalanced("What is) this?") === false);
+console.log(isBalanced("What (is this?") === false);
+console.log(isBalanced("((What) (is this))?") === true);
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log(isBalanced("Hey!") === true);
+console.log(isBalanced(")Hey!(") === false);
+console.log(isBalanced("What ((is))) up(") === false);
