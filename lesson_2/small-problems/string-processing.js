4088d2c211b6c9be8e66068b59e373109059885f
Rileypj <11priley@gmail.com>
2024-10-04 16:50:18 -0400

    working on twenty one game

--- lesson_2/small-problems/string-processing.js
+++ lesson_2/small-problems/string-processing.js
@@ -129,3 +129,47 @@ console.log(
   staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
 );
 */
+/*
+// How Long Are You?
+function wordLengths(str = '') {
+  if (str.length === 0) {
+    return [];
+  }
+  let lengthArray = str.split(' ').map(word => {
+    return word + ' ' + word.length;
+  });
+  console.log(lengthArray);
+  return lengthArray;
+}
+
+wordLengths('cow sheep chicken');
+// ["cow 3", "sheep 5", "chicken 7"]
+
+wordLengths('baseball hot dogs and apple pie');
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+wordLengths("It ain't easy, is it?");
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+wordLengths('Supercalifragilisticexpialidocious');
+// ["Supercalifragilisticexpialidocious 34"]
+
+wordLengths('');      // []
+wordLengths();        // []
+*/
+
+// Search Word (Part 1)
+function searchWord(word, str) {
+  let highlightedText = str.split(' ').map(currentWord => {
+    if (currentWord.toLowerCase() === word.toLowerCase()) {
+      return "**" + currentWord.toUpperCase() + '**';
+    } else {
+      return currentWord;
+    }
+  }).join(' ');
+  console.log(highlightedText);
+  return highlightedText;
+}
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+searchWord('sed', text);
--- lesson_3/twenty-one.js
+++ lesson_3/twenty-one.js
@@ -10,6 +10,12 @@
 7. Compare cards and declare winner.
 */
 const rlSync = require('readline-sync');
+const CARD_NAMES = {
+  J: 'Jack',
+  Q: 'Queen',
+  K: 'King',
+  A: 'Ace'
+};
 
 function makeDeck() {
   let arr = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
@@ -27,8 +33,39 @@ function shuffle(array) {
   return array;
 }
 
+function calculateAcesValue(currentPlayerCards) {
+  // if there's only one Ace, check the total + 11 > 21, the value of Ace should be set to 1
+  // any new aces, check if 1 + 11 + total is > 21, and if so, both Aces should be set to 1
+  let filterOutAces = Object.values(currentPlayerCards).filter(card => card!== 'A');
+  let currentAces = Object.values(currentPlayerCards).filter(card => card === 'A').length;
+  let total = getTotalValue(filterOutAces);
+
+  if (currentAces === 0 && total <= 10) {
+    return 10;
+  } 
+  if (total + (11 * currentAces) < 21) {
+    return total + (11 * currentAces) + 11 < 21 ? 11: 1;
+  } else {
+    return total + 1 + 11 < 21 ? 11: 1;
+  }
+}
+
+
 function convertCardsToValues (currentPlayerCards) {
-  //
+  // 1. If it's a number on the card, leave the number
+  // 2. Face cards = 10 
+  // 3. For Aces "A", look at the current total. Run the calculate Aces function
+  let cards = Object.values(currentPlayerCards);
+  cards = cards.map(card => {
+    if (card > 0 && card < 10) {
+      return card;
+    } else if (['J','Q','K'].includes(card)) {
+      return 10;
+    } else {
+      return calculateAcesValue(cards);
+    }
+  })
+  return cards;
 }
 
 function deal(deck, player1, player2) {
@@ -48,45 +85,86 @@ is randomly shuffled, take the deck and select the first card
   assignCards(player1);
   assignCards(player2);
 }
-
-
-function getTotalValue(currentPlayer) {
-  return Object.values(currentPlayer).reduce((acc, card) => acc + card);
+function joinAnd(playersCards) {
+  let finalPhrase  = '';
+  playersCards.forEach((card, idx) => {
+    if (idx === playersCards.length-2) {
+      finalPhrase += (card + ' and ');
+    } else {
+      idx === playersCards.length - 1 ? finalPhrase += card : finalPhrase + card + ', ';
+    }
+  });
+  return finalPhrase;
 }
 
-function busted(currentPlayer) {
-  let values = getTotalValue(currentPlayer);
+function nameFaceCards(arr) {
+  arr = arr.map(card => {
+    if (['J','Q','K','A'].includes(card)) {
+      card = CARD_NAMES[card];
+      return card;
+    } else {
+      return card;
+    }
+  });
+  return arr;
+}
+
+function displayDeal(currentPlayer, name) {
+  let cards = Object.values(currentPlayer);
+  cards = nameFaceCards(cards);
+  if (name === 'dealer') {
+    console.log(`Dealer has ${cards[0]} and unknown card`)
+  } else {
+    console.log(`You have: ${joinAnd(cards)}`);
+  }
+}
+
+function getTotalValue(currentPlayer) {
+  let converted = convertCardsToValues(currentPlayer);
+  return converted.reduce((acc, card) => acc + card, 0);
+}
+
+function busted(currentPlayerValues) {
+  let values = getTotalValue(currentPlayerValues);
   return values > 21;
 }
 
-// while (true) {
-  // const deck = shuffle(makeDeck());
+while (true) {
+  const deck = shuffle(makeDeck());
+  const player = {};
+  const dealer = {};
 
-  // const player = {};
+  deal(deck, player, dealer);
+  console.log({player});
+  displayDeal(dealer, 'dealer');
+  displayDeal(player, 'player');
   // let totalPlayer = getTotalValue(player);
-  // const dealer = {};
   // let totalDealer = getTotalValue(dealer);
-  // deal(deck, player, dealer);
-
-//   /*
-//   Player Turn
-//   1. Ask player to hit or stay.
-//   2. If stay, stop asking.
-//   3. Otherwise, go to #1.
-//   */
-//   while (true) {
-//     console.log("hit or stay?");
-//     let answer = rlSync.question();
-//     if (answer.toLowerCase() === 'stay' || busted()) break;
-//   }
-//   if (busted()) {
-//     // probably end the game? or ask the user to play again?
-//   } else {
-//     console.log("You chose to stay!");  // if player didn't bust, must have stayed to get here
-//   }
+  
+  /*
+  Player Turn
+  1. Ask player to hit or stay.
+  2. If stay, stop asking.
+  3. Otherwise, go to #1.
+  */
+ // currently this is resetting my deck of cards after each loop and not remembering what cards I have
+  while (true) {
+    console.log("hit or stay?");
+    let answer = rlSync.question();
+    console.log({player});
+    if (answer.toLowerCase() === 'stay' || busted(getTotalValue(player))) break;
+    player[`card${Object.values(player).length + 1}`] = deck.shift();
+    displayDeal(player, 'player');
+  }
+  // if (busted()) {
+  //   // probably end the game? or ask the user to play again?
+  //   console.log('Oh no! Looks like you went over 21. Dealer wins. Would you like to play again?')
+  // } else {
+  //   console.log("You chose to stay!");  // if player didn't bust, must have stayed to get here
+  // }
 
   /*
   Dealer Turn
 
   */
-// }
+}
