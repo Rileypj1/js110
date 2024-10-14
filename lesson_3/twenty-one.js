ac794bbf32d6a29c29267fdcffefd805037cd28e
Rileypj <11priley@gmail.com>
2024-10-14 18:35:36 -0400

    refactoring twenty-one

--- lesson_3/twenty-one.js
+++ lesson_3/twenty-one.js
@@ -16,14 +16,21 @@ const CARD_NAMES = {
   K: 'King',
   A: 'Ace'
 };
+const SUITS = ['S','H','D','C'];
+const POINT_THRESHOLD = 21;
+const DEALER_POINTS_UNTIL_STAYING = 17;
 
 function makeDeck() {
   let arr = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
-  for (let i = 1; i < 3; i += 1) {
-    arr = arr.concat(arr);
+  let finalDeck = [];
+  for (let suit = 0; suit < 4; suit += 1) {
+    arr.forEach(cardValue => {
+      finalDeck.push([SUITS[suit],cardValue]);
+    })
   }
-  return arr;
+  return finalDeck;
 }
+
 //shuffle cards in random order
 function shuffle(array) {
   for (let index = array.length - 1; index > 0; index--) {
@@ -34,49 +41,42 @@ function shuffle(array) {
 }
 
 function calculateAcesValue(currentPlayerCards) {
-  // if there's only one Ace, check the total + 11 > 21, the value of Ace should be set to 1
-  // any new aces, check if 1 + 11 + total is > 21, and if so, both Aces should be set to 1
-  let filterOutAces = currentPlayerCards.filter(card => card !== 'A');
-  let currentAces = currentPlayerCards.filter(card => card === 'A').length;
-  let total = filterOutAces.reduce((acc, cardValue) => acc + cardValue, 0);
+  let filterOutAces = currentPlayerCards.filter(card => card[1] !== 'A');
+  let currentAces = currentPlayerCards.filter(card => card[1] === 'A').length;
+  let total = filterOutAces.reduce((acc, cardValue) => acc + cardValue[1], 0);
 
   if (currentAces === 0 && total <= 10) {
     return 10;
-  } 
-  if (total + (11 * currentAces) < 21) {
-    return total + (11 * currentAces) + 11 < 21 ? 11: 1;
+  }
+  if (total + (11 * currentAces) <= POINT_THRESHOLD) {
+    return total + (11 * currentAces) + 11 <= POINT_THRESHOLD ? 11 : 1;
   } else {
-    return total + 1 + 11 < 21 ? 11: 1;
+    return total + 1 + 11 <= POINT_THRESHOLD ? 11 : 1;
   }
 }
 
 
 function convertCardsToValues (currentPlayerCards) {
   // 1. If it's a number on the card, leave the number
-  // 2. Face cards = 10 
+  // 2. Face cards = 10
   // 3. For Aces "A", look at the current total. Run the calculate Aces function
   let cards = Object.values(currentPlayerCards);
   cards = cards.map(card => {
-    if (card >= 2 && card <= 10) {
-      return card;
-    } else if (['J','Q','K'].includes(card)) {
+    if (card[1] >= 2 && card[1] <= 10) {
+      return card[1];
+    } else if (['J','Q','K'].includes(card[1])) {
       return 10;
     } else {
       return calculateAcesValue(cards);
     }
-  })
+  });
   return cards;
 }
 
 function deal(deck, player1, player2) {
-/*
-1. since the deck at this point
-is randomly shuffled, take the deck and select the first card
-2. assign it to card1 with the property of the value of the card
 
-*/
   function assignCards(selectedPlayer) {
-    let counter = 1
+    let counter = 1;
     while (counter <= 2) {
       selectedPlayer[`card${counter}`] = deck.shift();
       counter += 1;
@@ -85,13 +85,14 @@ is randomly shuffled, take the deck and select the first card
   assignCards(player1);
   assignCards(player2);
 }
+
 function joinAnd(playersCards) {
   let finalPhrase  = '';
   playersCards.forEach((card, idx) => {
-    if (idx === playersCards.length-2) {
+    if (idx === playersCards.length - 2) {
       finalPhrase += (card + ' and ');
     } else {
-      idx === playersCards.length - 1 ? finalPhrase += card : finalPhrase += card + ', ';
+      finalPhrase += idx === playersCards.length - 1 ? card : card + ', ';
     }
   });
   return finalPhrase;
@@ -99,9 +100,9 @@ function joinAnd(playersCards) {
 
 function nameFaceCards(arr) {
   arr = arr.map(card => {
-    if (['J','Q','K','A'].includes(card)) {
-      card = CARD_NAMES[card];
-      return card;
+    if (['J','Q','K','A'].includes(card[1])) {
+      let name = CARD_NAMES[card[1]];
+      return [card[0],name];
     } else {
       return card;
     }
@@ -109,11 +110,13 @@ function nameFaceCards(arr) {
   return arr;
 }
 
-function displayDeal(currentPlayer, name) {
+function displayDeal(currentPlayer, name, mask = 'n') {
   let cards = Object.values(currentPlayer);
   cards = nameFaceCards(cards);
-  if (name === 'dealer') {
-    console.log(`Dealer has ${cards[0]} and unknown card`)
+  if (name === 'dealer' && mask === 'y') {
+    console.log(`Dealer has ${cards[0]} and unknown card`);
+  } else if (name === 'dealer' && mask === 'n') {
+    console.log(`Dealer has ${joinAnd(cards)}`);
   } else {
     console.log(`You have ${joinAnd(cards)}`);
   }
@@ -123,16 +126,16 @@ function getTotalValue(currentPlayer) {
   let converted = convertCardsToValues(currentPlayer);
   return converted.reduce((acc, card) => acc + card, 0);
 }
-// let player = { card1: 7, card2: 10, card4: 'Q' };
-// console.log(convertCardsToValues(player));
+
 
 function busted(totalCardValue) {
-  return totalCardValue > 21;
+  return totalCardValue > POINT_THRESHOLD;
 }
+
 
 function doesDealerHitOrStay(currentPlayer) {
   let dealerCurrentTotal = getTotalValue(currentPlayer);
-  if (dealerCurrentTotal >= 17) {
+  if (dealerCurrentTotal >= DEALER_POINTS_UNTIL_STAYING) {
     return 'stay';
   } else {
     return 'hit';
@@ -142,15 +145,21 @@ function doesDealerHitOrStay(currentPlayer) {
 function displayWinner(player1, player2) {
   let player1Total = getTotalValue(player1);
   let player2Total = getTotalValue(player2);
-  let dealerCards = nameFaceCards(player2);
-  let winner = (player1Total > player2Total) && (player1Total !== player2Total) ? 'You': 'Dealer';
+  let winner = (player1Total > player2Total) && (player1Total !== player2Total) ? 'You' : 'Dealer';
 
   if (player1Total === player2Total) {
-    console.log(`\nYou had: ${displayDeal(player1, 'player')}. Your total: ${player1Total}.`);
-    console.log(`The dealer had ${joinAnd(dealerCards)}. The dealer's total: ${player2Total}. It's a tie!`);
+    displayDeal(player1, 'player');
+    console.log(`Your total: ${player1Total}.\n`);
+    displayDeal(player2,'dealer');
+    console.log(`The dealer's total: ${player2Total}.\n`);
+    console.log(`It's a tie!`);
   } else {
-    console.log(`\nYou had: ${displayDeal(player1, 'player')}. Your total: ${player1Total}.`);
-    console.log(`The dealer had ${joinAnd(dealerCards)}. The dealer's total: ${player2Total}. ${winner} won!`);
+    displayDeal(player1, 'player');
+    console.log(`Your total: ${player1Total}.\n`);
+    displayDeal(player2,'dealer');
+    console.log(`The dealer's total: ${player2Total}.\n`);
+
+    console.log(`${winner} won!`);
   }
 }
 
@@ -160,10 +169,10 @@ function runDealerTurn(dealer, deck) {
     let decision = doesDealerHitOrStay(dealer);
 
     console.log(`Dealer chose ${decision}`);
-    if (decision === 'stay') {
-      return 'stay';
-    } else if (busted(getTotalValue(dealer))) {
+    if (busted(getTotalValue(dealer))) {
       return 'busted';
+    } else if (decision === 'stay') {
+      return 'stay';
     }
     dealer[`card${Object.values(dealer).length + 1}`] = deck.shift();
     displayDeal(dealer, 'dealer');
@@ -174,29 +183,29 @@ function displayBustedMessage(bool, inputPlayer, playerName) {
   if (bool) {
     displayDeal(inputPlayer, playerName);
     let winner = playerName === 'player' ? 'Dealer' : 'Player';
-    // probably end the game? or ask the user to play again?
     console.log(`\nLooks like you went over 21! ${winner} wins.`);
   }
 }
 function runPlayerTurn(player, dealer, deck) {
   while (true) {
     console.log("hit or stay?" + '\n');
-    answer = rlSync.question();
+    let answer = rlSync.question();
     if (answer.toLowerCase() === 'stay') return 'stay';
     player[`card${Object.values(player).length + 1}`] = deck.shift();
     if (busted(getTotalValue(player))) return 'busted';
     displayDeal(player, 'player');
-    displayDeal(dealer, 'dealer');
-  };
+    displayDeal(dealer, 'dealer','y');
+  }
 }
 function playAgainAndCheckResponse(message) {
   let str = rlSync.question(message);
-  while (!['y','n','Y', 'N', 'Yes'.toLowerCase(), 'No'.toLowerCase()].includes(str.trim())) {
+  while (!['y','n','Y', 'N', 'yes', 'no'].includes(str.trim().toLowerCase())) {
     str = rlSync.question(message);
   }
   return str.toLowerCase();
 }
 
+// game play
 while (true) {
   console.clear();
   const deck = shuffle(makeDeck());
@@ -206,33 +215,24 @@ while (true) {
 
   deal(deck, player, dealer);
   displayDeal(player, 'player');
-  displayDeal(dealer, 'dealer');
+  displayDeal(dealer, 'dealer','y');
 
-  /*
-  Player Turn
-  1. Ask player to hit or stay.
-  2. If stay, stop asking.
-  3. Otherwise, go to #1.
-  */
-
+  // player turn
   let outcome = runPlayerTurn(player, dealer, deck);
-  let dealerOutcome = ''
+  let dealerOutcome = '';
   if (outcome === 'busted') {
     displayBustedMessage(busted(getTotalValue(player)), player, 'player');
-    answer = playAgainAndCheckResponse('Would you like to play again? Please type y/n ');
   } else {
-      // Dealer Turn
+    // Dealer Turn
     dealerOutcome = runDealerTurn(dealer, deck);
     if (answer === 'n') break;
     if (dealerOutcome === 'busted') {
-      displayDeal(dealer, 'dealer')
+      displayDeal(dealer, 'dealer');
       console.log('You won! Looks like the dealer was busted.');
-      answer = playAgainAndCheckResponse('Would you like to play again? (y/n)');
     } else {
       displayWinner(player, dealer);
-      answer = playAgainAndCheckResponse('Would you like to play again? (y/n) ');
     }
   }
-
-  if(answer === 'n') break;
+  answer = playAgainAndCheckResponse('Would you like to play again? (y/n) ');
+  if (answer === 'n') break;
 }
